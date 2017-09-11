10d92ebe5c3281806353bd507e6c2731069b3469
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-09-11 18:33:25 -0400

    Updated Dependencies and Initialized Libraries

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+var express = require('express'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 const Web3 = require('web3');
-var web3 = new Web3();
 
+
+var web3 = new Web3();
 const web3Admin = require('web3admin');
 web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8101'))
 
@@ -9,7 +14,25 @@ setTimeout(function(){
     console.log("Extended");
     console.log(web3.admin.nodeInfo)
     console.log(web3.admin.peers)
- }, 1000)
+ }, 1000);
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+//Starting the application on port 8080
+app.listen(8080);
+console.log("App listening on port 8080");
 
 console.log("App started!");
 console.log(web3.eth.accounts);
